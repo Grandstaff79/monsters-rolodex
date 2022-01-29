79b2b35cda7931870ed5ec732f6436bcec9f53cc
Grandstaff79 <48068304+Grandstaff79@users.noreply.github.com>
2022-01-29 12:20:28 -0500

    updated searchbox

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
 
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -9,7 +11,9 @@ class App extends Component {
       super();
 
       this.state = {
-        monsters:  [ ]
+        monsters:  [ ],
+        searchField: ' '
+
       };
     }
 
@@ -19,9 +23,17 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
     }
     render() {
+      const { monsters, searchField} = this.state;
+      const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
       return (
           <div className="App">
-          <CardList monsters={this.state.monsters}/>
+        
+            <SearchBox
+            placeholder='search monsters'  
+            handleChange={ e => this.setState({ searchField: e.target.value})}
+
+            />
+          <CardList monsters={filteredMonsters}/>
            </div>
             );
           }
